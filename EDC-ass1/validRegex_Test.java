4ea18358824aff97ea6466f4c719946a7c58119c
Spencer <a1765159@adelaide.edu.au>
2022-08-22 20:15:15 +0930

    implemented parameterized testing

--- EDC-ass1/validRegex_Test.java
+++ EDC-ass1/validRegex_Test.java
@@ -1,82 +1,49 @@
 import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 // compile with: javac -cp .:junit-4.13.2.jar:hamcrest-core-1.3.jar *_Test.java
 //     run with: java -cp .:junit-4.13.2.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore validRegex_Test
 
+@RunWith(Parameterized.class)
 public class validRegex_Test {
-  @Test
-  public void test1() {
-    assertEquals( true, RegexEngine.validRegex("abcde fghijklmnopqr stuvwxyz") );
+  @Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+              { "abcde fghijklmnopqr stuvwxyz", true },
+              { "ABCD EFGHI JKLMNOPQR STUVWXYZ", true },
+              { "012 345 6789", true },
+              { "( | * + )", true },
+              { "Hello\nworld", false },
+              { "(a3b)*|c+", true },
+              { "(ab)*|c-", false },
+              { "gray|grey", true },
+              { "gr[ae]y", false },
+              { "colou2?r", false },
+              { "go+gle", true },
+              { "z{3,6}", false },
+              { "(a|b)(c|d)", true },
+              { "(a|b|c)*     1+ | 9", true },
+              { "Fi(ni+)*|te 5(t*a+t(e)) Au(t)(0|m|a|t*|(a+))", true }
+    });
+  }
+
+  private String pInput;
+  private boolean pExpected;
+
+  public validRegex_Test(String input, boolean expected) {
+    this.pInput = input;
+    this.pExpected = expected;
   }
 
   @Test
-  public void test2() {
-    assertEquals( true, RegexEngine.validRegex("ABCD EFGHI JKLMNOPQR STUVWXYZ") );
-  }
-
-  @Test
-  public void test3() {
-    assertEquals( true, RegexEngine.validRegex("012345 6789") );
-  }
-
-  @Test
-  public void test4() {
-    assertEquals( true, RegexEngine.validRegex("( | * + )") );
-  }
-
-  @Test
-  public void test5() {
-    assertEquals( false, RegexEngine.validRegex("Hello\nworld") );
-  }
-
-  @Test
-  public void test6() {
-    assertEquals( true, RegexEngine.validRegex("(ab)*|c+") );
-  }
-
-  @Test
-  public void test7() {
-    assertEquals( false, RegexEngine.validRegex("(ab)*|c-") );
-  }
-
-  @Test
-  public void test8() {
-    assertEquals( true, RegexEngine.validRegex("gray|grey") );
-  }
-
-  @Test
-  public void test9() {
-    assertEquals( false, RegexEngine.validRegex("gr[ae]y") );
-  }
-
-  @Test
-  public void test10() {
-    assertEquals( false, RegexEngine.validRegex("colou?r") );
-  }
-
-  @Test
-  public void test11() {
-    assertEquals( true, RegexEngine.validRegex("go+gle") );
-  }
-
-  @Test
-  public void test12() {
-    assertEquals( false, RegexEngine.validRegex("colou?r") );
-  }
-
-  @Test
-  public void test13() {
-    assertEquals( false, RegexEngine.validRegex("z{3,6}") );
-  }
-
-  @Test
-  public void test14() {
-    assertEquals( true, RegexEngine.validRegex("(a|b)(c|d") );
-  }
-
-  @Test
-  public void test15() {
-    assertEquals( true, RegexEngine.validRegex("colou?r") );
+  public void test() {
+    assertEquals(pExpected, RegexEngine.validRegex(pInput) );
   }
 }
