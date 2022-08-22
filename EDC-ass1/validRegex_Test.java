38fdd51b1702596bf76969a33ccc51d14c815030
Spencer <a1765159@adelaide.edu.au>
2022-08-22 19:48:53 +0930

    tests for validRegex

--- EDC-ass1/validRegex_Test.java
+++ EDC-ass1/validRegex_Test.java
@@ -7,7 +7,76 @@ import org.junit.Test;
 public class validRegex_Test {
   @Test
   public void test1() {
-    boolean valid = RegexEngine.validRegex("1+2+3");
-    assertEquals(true, valid);
+    assertEquals( true, RegexEngine.validRegex("abcde fghijklmnopqr stuvwxyz") );
+  }
+
+  @Test
+  public void test2() {
+    assertEquals( true, RegexEngine.validRegex("ABCD EFGHI JKLMNOPQR STUVWXYZ") );
+  }
+
+  @Test
+  public void test3() {
+    assertEquals( true, RegexEngine.validRegex("012345 6789") );
+  }
+
+  @Test
+  public void test4() {
+    assertEquals( true, RegexEngine.validRegex("( | * + )") );
+  }
+
+  @Test
+  public void test5() {
+    assertEquals( false, RegexEngine.validRegex("Hello\nworld") );
+  }
+
+  @Test
+  public void test6() {
+    assertEquals( true, RegexEngine.validRegex("(ab)*|c+") );
+  }
+
+  @Test
+  public void test7() {
+    assertEquals( false, RegexEngine.validRegex("(ab)*|c-") );
+  }
+
+  @Test
+  public void test8() {
+    assertEquals( true, RegexEngine.validRegex("gray|grey") );
+  }
+
+  @Test
+  public void test9() {
+    assertEquals( false, RegexEngine.validRegex("gr[ae]y") );
+  }
+
+  @Test
+  public void test10() {
+    assertEquals( false, RegexEngine.validRegex("colou?r") );
+  }
+
+  @Test
+  public void test11() {
+    assertEquals( true, RegexEngine.validRegex("go+gle") );
+  }
+
+  @Test
+  public void test12() {
+    assertEquals( false, RegexEngine.validRegex("colou?r") );
+  }
+
+  @Test
+  public void test13() {
+    assertEquals( false, RegexEngine.validRegex("z{3,6}") );
+  }
+
+  @Test
+  public void test14() {
+    assertEquals( true, RegexEngine.validRegex("(a|b)(c|d") );
+  }
+
+  @Test
+  public void test15() {
+    assertEquals( true, RegexEngine.validRegex("colou?r") );
   }
 }
