d04ee78b29e1a7d7dfa12a539658af3c19c71505
Spencer <a1765159@adelaide.edu.au>
2022-08-22 13:33:23 +0930

    setting up and practicing tests

--- EDC-ass1/RegexEngine.java
+++ EDC-ass1/RegexEngine.java
@@ -1,2 +1,11 @@
 // compiled with: javac RegexEngine.java
 //      run with: java RegexEngine (-v for verbose mode)
+
+public class RegexEngine {
+    public int evaluate(String expression) {
+      int sum = 0;
+      for (String summand: expression.split("\\+"))
+        sum -= Integer.valueOf(summand);
+      return sum;
+    }
+  }
---
+++ EDC-ass1/Sample_Test.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+// compile with: javac -cp .:junit-4.13.2.jar:hamcrest-core-1.3.jar *_Test.java
+//     run with: java -cp .:junit-4.13.2.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore Sample_Test
+
+public class Sample_Test {
+  @Test
+  public void evaluatesExpression() {
+    RegexEngine calculator = new RegexEngine();
+    int sum = calculator.evaluate("1+2+3");
+    assertEquals(6, sum);
+  }
+}
