dc298dee5c49d458009376acba1cffdc10f3134e
Spencer <a1765159@adelaide.edu.au>
2022-08-22 17:23:47 +0930

    input validation

--- EDC-ass1/RegexEngine.java
+++ EDC-ass1/RegexEngine.java
@@ -6,17 +6,18 @@ import java.io.InputStreamReader;
 //      run with: java RegexEngine (-v for verbose mode)
 
 public class RegexEngine {
-    public static void main(String[] args) {
-        // take in regex expression
+    // main function
+    public static void main(String[] args) 
+        throws IOException 
+    {
         BufferedReader reader = new BufferedReader( new InputStreamReader(System.in) );
-        String regex = "";
-        try {
-            regex = reader.readLine();
+
+        // get regular expression to test
+        String regex = reader.readLine();
+        if ( !validRegex(regex) ) {
+            System.out.println("Error: This input is invalid: " + regex);
+            System.exit(1);
         }
-        catch(IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(regex);
 
         // NFA = createNFA(regex);
         // if(verbose) System.out.println( getTable(NFA) ); 
@@ -24,17 +25,29 @@ public class RegexEngine {
         
         // read each line until ctrl+c
         while (true) {
-            String line = "";
-            try {
-                line = reader.readLine();
-            }
-            catch(IOException e) {
-                e.printStackTrace();
-            }
-            System.out.println(line);
+            String line = reader.readLine();
 
-            // if( simulateNFA(NFA, line) ) System.out.println("true");
+            // if ( simulateNFA(NFA, line) ) System.out.println("true");
             // else System.out.println("false");
         }
+    }
+
+    // checks if regex string is acceptable
+    static boolean validRegex(String regex) {
+        for ( int i=0; i < regex.length(); i++ ) {
+            char ch = regex.charAt(i);
+            if ( !Character.isAlphabetic(ch) && !validSymbol(ch) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // checks if character is acceptable
+    static boolean validSymbol(char ch) {
+        if ( ch == '|' || ch == '*' || ch == '+' || ch == '(' || ch == ')' || ch == ' ' ) {
+            return true;
+        }
+        return false;
     }
 }
