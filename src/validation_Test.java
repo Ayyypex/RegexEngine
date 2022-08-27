e14ec381f7c89a5d89adc17b8b8ff7ecf49a7247
Spencer <a1765159@adelaide.edu.au>
2022-08-27 15:45:46 +0930

    testing for validation function

---
+++ src/validation_Test.java
@@ -0,0 +1,112 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+
+// compile with: javac -cp .:junit-4.13.2.jar:hamcrest-core-1.3.jar *_Test.java
+//     run with: java -cp .:junit-4.13.2.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore validRegex_Test
+
+public class validation_Test {
+
+  @Test
+  public void validChar_Test() {
+    Object[][] input =  {
+      { 'a', true },
+      { 'c', true },
+      { 'z', true },
+      { 'A', true },
+      { 'C', true },
+      { 'Z', true },
+      { '0', true },
+      { '9', true },
+      { ' ', true },
+      { '(', false },
+      { ')', false },
+      { '*', false },
+      { '+', false },
+      { '|', false },
+      { '_', false },
+      { '&', false },
+    };
+
+    for ( int i=0; i < input.length; i++ ) {
+      assertEquals( input[i][1], RegexEngine.validChar( String.valueOf(input[i][0]).charAt(0) ) );
+    }
+  }
+
+  @Test
+  public void validSymbol_Test() {
+    Object[][] input =  {
+      { '(', true },
+      { ')', true },
+      { '*', true },
+      { '+', true },
+      { '|', true },
+      { 'a', false },
+      { 'c', false },
+      { 'z', false },
+      { 'A', false },
+      { 'C', false },
+      { 'Z', false },
+      { '0', false },
+      { '9', false },
+      { ' ', false },
+      { '_', false },
+      { '&', false },
+    };
+
+    for ( int i=0; i < input.length; i++ ) {
+      assertEquals( input[i][1], RegexEngine.validSymbol( String.valueOf(input[i][0]).charAt(0) ) );
+    }
+  }
+
+  @Test
+  public void precedenceOf_Test() {
+    Object[][] input =  {
+      { '*', 2 },
+      { '+', 2 },
+      { '_', 1 },
+      { '|', 0 },
+      { 'a', 0 },
+      { 'c', 0 },
+      { 'z', 0 },
+      { 'A', 0 },
+      { 'C', 0 },
+      { 'Z', 0 },
+      { '0', 0 },
+      { '9', 0 },
+      { ' ', 0 },
+    };
+
+    for ( int i=0; i < input.length; i++ ) {
+      assertEquals( input[i][1], RegexEngine.precedenceOf( String.valueOf(input[i][0]).charAt(0) ) );
+    }
+  }
+
+  @Test
+  public void validRegex_Test() {
+    Object[][] input = {
+      { "abcde fghijklmnopqr stuvwxyz", true },
+      { "ABCD EFGHI JKLMNOPQR STUVWXYZ", true },
+      { "012 345 6789", true },
+      { "( | * + )", true },
+      { "Hello\nworld", false },
+      { "(a3b)*|c+", true },
+      { "(ab)*|c-", false },
+      { "gray|grey", true },
+      { "gr[ae]y", false },
+      { "colou2?r", false },
+      { "go+gle", true },
+      { "z{3,6}", false },
+      { "(a|b)(c|d)", true },
+      { "(a|b|c)*     1+ | 9", true },
+      { "Fi(ni+)*|te 5(t*a+t(e)) Au(t)(0|m|a|t*|(a+))", true }
+    };
+
+    for ( int i=0; i < input.length; i++ ) {
+      assertEquals( input[i][1], RegexEngine.validRegex( String.valueOf(input[i][0]) ) );
+    }
+  }
+}
